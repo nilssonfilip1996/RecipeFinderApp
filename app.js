6353f62045215d327c9fcca923e92c75cfd7a28f
nilssonfilip1996 <nilssonfilip1996@hotmail.com>
2025-01-17 12:10:44 +0100

    QoL improvements: Remember the choices from the dropdown-menues when hitting different routes.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import bodyParser from "body-parser"
 import dotenv from "dotenv"
 import * as fs from 'fs';
 import { PROTEIN_LIST, COUSINE_LIST} from "./public/constants/searchParameters.js";
+import { log } from "console";
 dotenv.config();
 
 const app = express();
@@ -11,21 +12,36 @@ const port = 3000;
 const API_URL = "https://api.spoonacular.com";
 const apiKey = process.env.API_KEY;
 
+let userSearch = {
+    "cousine":"",
+    "protein":"",
+    "searchResults":[]
+}
+
 app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-    res.render("index.ejs", {cousines: COUSINE_LIST, proteins: PROTEIN_LIST});
+    userSearch = {
+        "cousine":"",
+        "protein":"",
+        "searchResults":[]
+    }
+    res.render("index.ejs", {cousines: COUSINE_LIST, proteins: PROTEIN_LIST, userSearch});
 
 });
 
 app.post("/complexSearch", async (req, res) => {
     console.log(req.body);
     var recipeList = await axios.get(`${API_URL}/recipes/complexSearch?apiKey=${apiKey}&cuisine=${req.body.selectedCousine}&includeIngredients=${req.body.selectedProtein}&number=9`);
-    console.log(recipeList.data.results);
+    //console.log(recipeList.data.results);
+    userSearch.cousine = req.body.selectedCousine;
+    userSearch.protein = req.body.selectedProtein;
+    userSearch.searchResults = recipeList.data.results;
+    console.log(userSearch);
     
-    res.render("index.ejs", {cousines: COUSINE_LIST, proteins: PROTEIN_LIST, searchResults: recipeList.data.results});
+    res.render("index.ejs", {cousines: COUSINE_LIST, proteins: PROTEIN_LIST, userSearch});
 
     //Dummy data to reduce the amount of API-requests during testing.
 /*     let openJSON = fs.readFileSync('dummyEntries.json', 'utf-8');
@@ -33,17 +49,15 @@ app.post("/complexSearch", async (req, res) => {
     res.render("index.ejs", {cousines: COUSINE_LIST, proteins: PROTEIN_LIST, searchResults: recipeList}); */
 });
 
+app.post("/viewRecipe/:id", (req, res) => {
+    console.log(req.params.id);
+    let openJSON = fs.readFileSync('dummyEntries.json', 'utf-8');
+    let recipeList = JSON.parse(openJSON);
+    res.render("index.ejs", {cousines: COUSINE_LIST, proteins: PROTEIN_LIST, userSearch});
+});
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
   });
 
-  /* Populate the entries array with dummy entries. For testing purposes.*/
-  function populateDummyArray(){
-      let openJSON = fs.readFileSync('dummyEntries.json', 'utf-8');
-      let parseJSON = JSON.parse(openJSON);
-  
-      for(let i = 0; i < parseJSON.length; i++){ 
-          var item = parseJSON[i];
-          entries.push(new blogEntry(item.title, item.text, item.date));
-      }
-  };
+
