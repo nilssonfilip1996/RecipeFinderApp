c7767e9fe1645d5c12060bf7abc84b3dabb2f34c
nilssonfilip1996 <nilssonfilip1996@hotmail.com>
2025-02-28 20:56:21 +0100

    Moved Ejs code into seperate files. Changed the parameters used when rendering index.ejs into a more generic format. Now some of the ejs files should be easier to reuse later on.

--- src/routes/recipeRoutes.js
+++ src/routes/recipeRoutes.js
@@ -1,56 +1,80 @@
 import { Router } from "express";
-import { searchForRecipes, getRecipe } from "../utils/spoonacularHandler.js"
-import { PROTEIN_LIST, COUSINE_LIST} from "../../public/constants/searchParameters.js";
+import { searchForRecipes, getRecipe } from "../utils/spoonacularHandler.js";
+import {
+  PROTEIN_LIST,
+  COUSINE_LIST,
+} from "../../public/constants/searchParameters.js";
 
+const dropDownValues = { proteins: PROTEIN_LIST, cousines: COUSINE_LIST };
 const router = Router();
 
 /* Default route. */
 router.get("/", (req, res) => {
-    if(req.isAuthenticated()){
-        console.log(`${req.user.username} is online.`);
-    }
-    
-    /* Remember what the user enters so sumbsequent requests can auto-fill forms etc. */
-    req.session.searchParams = {
-        "cousine":"",
-        "protein":"",
-        "chosenRecipe":{},
-        "searchResults":[]
-    };
-    res.render("index.ejs", {cousines: COUSINE_LIST, proteins: PROTEIN_LIST, localSession: req.session.searchParams});
+  if (req.isAuthenticated()) {
+    console.log(`${req.user.username} is online.`);
+  }
+
+  /* Remember what the user enters so sumbsequent requests can auto-fill forms etc. */
+  req.session.currentState = {
+    selectedSearchParams: {cousine: "",protein: ""},
+    chosenRecipe: {},
+    searchResults: [],
+  };
+  res.render("index.ejs", {
+    dropDownValues: dropDownValues,
+    selectedSearchParams: req.session.currentState.selectedSearchParams,
+    currentState: req.session.currentState,
+  });
 });
 
 /* Post-request. Called when the user wants to find recipes based on cousine and protein */
 router.post("/recipeSearch", async (req, res) => {
-    try {
-        var recipeList = await searchForRecipes(req.body.selectedCousine, req.body.selectedProtein, 9);
+  try {
+    var recipeList = await searchForRecipes(
+      req.body.selectedCousine,
+      req.body.selectedProtein,
+      9
+    );
 
-        req.session.searchParams.cousine = req.body.selectedCousine;
-        req.session.searchParams.protein = req.body.selectedProtein;
-        req.session.searchParams.searchResults = recipeList.data.results;
+    req.session.currentState.selectedSearchParams.cousine = req.body.selectedCousine;
+    req.session.currentState.selectedSearchParams.protein = req.body.selectedProtein;
+    req.session.currentState.searchResults = recipeList.data.results;
 
-        var displayError = null;
-        if(req.session.searchParams.searchResults<1){
-            displayError = "No recipes found."
-        }
-        res.render("index.ejs", {cousines: COUSINE_LIST, proteins: PROTEIN_LIST, localSession: req.session.searchParams, error: displayError, scrollToResults: true});
-    } catch (error) {
-        console.log(JSON.stringify(error.response.data));
-        res.render("index.ejs", {error: error.response.data.message});
+    var displayError = null;
+    if (req.session.currentState.searchResults < 1) {
+      displayError = "No recipes found.";
     }
+    res.render("index.ejs", {
+      dropDownValues: dropDownValues,
+      selectedSearchParams: req.session.currentState.selectedSearchParams,
+      searchResults: req.session.currentState.searchResults,
+      selectedRecipe: req.session.currentState.chosenRecipe,
+      error: displayError,
+      scrollToResults: true,
+    });
+  } catch (error) {
+    console.log(JSON.stringify(error.response.data));
+    res.render("index.ejs", { error: error.response.data.message });
+  }
 });
 
 /* Post-request. Called when the user wants to view a specific recipe based on it's id.*/
 router.post("/viewRecipe/:id", async (req, res) => {
-    try {
-        var recipe = await getRecipe(req.params.id);
-        req.session.searchParams.chosenRecipe = recipe.data;
-        res.render("index.ejs", {cousines: COUSINE_LIST, proteins: PROTEIN_LIST, localSession: req.session.searchParams, recipe: recipe.data});
-    } catch (error) {
-        const stringifiedError = JSON.stringify(error.response.data);
-        console.log(stringifiedError);
-        res.render("index.ejs", {error: error.response.data.message});
-    }
+  try {
+    var recipe = await getRecipe(req.params.id);
+    req.session.currentState.chosenRecipe = recipe.data;
+    
+    res.render("index.ejs", {
+      dropDownValues: dropDownValues,
+      selectedSearchParams: req.session.currentState.selectedSearchParams,
+      searchResults: req.session.currentState.searchResults,
+      selectedRecipe: req.session.currentState.chosenRecipe,
+    });
+  } catch (error) {
+    const stringifiedError = JSON.stringify(error.response.data);
+    console.log(stringifiedError);
+    res.render("index.ejs", { error: error.response.data.message });
+  }
 });
 
 export default router;
