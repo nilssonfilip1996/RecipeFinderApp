2550b34176a7cf9cb7073584bf64d6f44683d878
nilssonfilip1996 <nilssonfilip1996@hotmail.com>
2025-02-28 12:32:52 +0100

    App is now using sessions to remember the users recent search-parameters. Seperated some of the front-end into seperate files.

--- src/routes/recipeRoutes.js
+++ src/routes/recipeRoutes.js
@@ -4,41 +4,36 @@ import { PROTEIN_LIST, COUSINE_LIST} from "../../public/constants/searchParamete
 
 const router = Router();
 
-let userSearch = {
-    "cousine":"",
-    "protein":"",
-    "chosenRecipe":{},
-    "searchResults":[]
-}
-
 /* Default route. */
 router.get("/", (req, res) => {
     if(req.isAuthenticated()){
         console.log(`${req.user.username} is online.`);
     }
-    userSearch = {
+    
+    /* Remember what the user enters so sumbsequent requests can auto-fill forms etc. */
+    req.session.searchParams = {
         "cousine":"",
         "protein":"",
         "chosenRecipe":{},
         "searchResults":[]
-    }
-    res.render("index.ejs", {cousines: COUSINE_LIST, proteins: PROTEIN_LIST, userSearch});
+    };
+    res.render("index.ejs", {cousines: COUSINE_LIST, proteins: PROTEIN_LIST, localSession: req.session.searchParams});
 });
 
 /* Post-request. Called when the user wants to find recipes based on cousine and protein */
 router.post("/recipeSearch", async (req, res) => {
-    //console.log(req.body);
     try {
         var recipeList = await searchForRecipes(req.body.selectedCousine, req.body.selectedProtein, 9);
-        userSearch.cousine = req.body.selectedCousine;
-        userSearch.protein = req.body.selectedProtein;
-        userSearch.searchResults = recipeList.data.results;
-        //console.log(req.body);
+
+        req.session.searchParams.cousine = req.body.selectedCousine;
+        req.session.searchParams.protein = req.body.selectedProtein;
+        req.session.searchParams.searchResults = recipeList.data.results;
+
         var displayError = null;
-        if(userSearch.searchResults<1){
+        if(req.session.searchParams.searchResults<1){
             displayError = "No recipes found."
         }
-        res.render("index.ejs", {cousines: COUSINE_LIST, proteins: PROTEIN_LIST, userSearch, error: displayError, scrollToResults: true});
+        res.render("index.ejs", {cousines: COUSINE_LIST, proteins: PROTEIN_LIST, localSession: req.session.searchParams, error: displayError, scrollToResults: true});
     } catch (error) {
         console.log(JSON.stringify(error.response.data));
         res.render("index.ejs", {error: error.response.data.message});
@@ -47,11 +42,10 @@ router.post("/recipeSearch", async (req, res) => {
 
 /* Post-request. Called when the user wants to view a specific recipe based on it's id.*/
 router.post("/viewRecipe/:id", async (req, res) => {
-    console.log(req.params.id);
     try {
         var recipe = await getRecipe(req.params.id);
-        userSearch.chosenRecipe = recipe.data;
-        res.render("index.ejs", {cousines: COUSINE_LIST, proteins: PROTEIN_LIST, userSearch, recipe: recipe.data});
+        req.session.searchParams.chosenRecipe = recipe.data;
+        res.render("index.ejs", {cousines: COUSINE_LIST, proteins: PROTEIN_LIST, localSession: req.session.searchParams, recipe: recipe.data});
     } catch (error) {
         const stringifiedError = JSON.stringify(error.response.data);
         console.log(stringifiedError);
--- src/utils/spoonacularHandler.js
+++ src/utils/spoonacularHandler.js
@@ -5,6 +5,9 @@ const API_URL = "https://api.spoonacular.com";
 
 /* 
     Returns recipes that match the criterias passed as parameters.
+    Params:
+        maxResults: The maximum number of results that the api should return.
+        sortOption: Sort the search results. Default value: healthiness.
 */
 export function searchForRecipes(cousine, protein, maxResults, sortOption="healthiness"){
     const API_KEY = process.env.API_KEY;
@@ -13,6 +16,8 @@ export function searchForRecipes(cousine, protein, maxResults, sortOption="healt
 
 /* 
     Returns recipe-information matching the input id.
+    Params:
+        id: The id of the recipe on Spoonacular.com
 */
 export function getRecipe(id) {
     const API_KEY = process.env.API_KEY;
